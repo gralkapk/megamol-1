7fa8e2dc1a3a8ce3df8237b223916523792b8ea7
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2024-06-13 10:37:26 +0200

    adaptive subdivision

--- plugins/optix_hpg/src/optix/CTreelets.h
+++ plugins/optix_hpg/src/optix/CTreelets.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stack>
+#include <tuple>
 #include <unordered_map>
 #include <vector>
 
@@ -47,6 +49,9 @@ inline void ctreelets_partition(
     glm::uvec3 scaled_radius =
         glm::uvec3((radius / span.x) * factor, (radius / span.y) * factor, (radius / span.z) * factor);
 
+    std::stack<std::tuple<size_t, size_t, int>> remaining_jobs;
+    int offset = 10;
+
     partitionRecursively<device::FPKDParticle, device::box3u32>(
         fparticles, 0ULL, fparticles.size(), [&](size_t begin, size_t end, bool force, device::box3u32 const& bounds) {
             /*bool makeLeaf() :*/
@@ -58,25 +63,69 @@ inline void ctreelets_partition(
             std::unordered_map<uint32_t, uint32_t> y_counter;
             std::unordered_map<uint32_t, uint32_t> z_counter;
             for (size_t i = begin; i < end; ++i) {
-                ++x_counter[(fparticles[i].pos.x - bounds.lower.x) >> 12];
-                ++y_counter[(fparticles[i].pos.y - bounds.lower.y) >> 12];
-                ++z_counter[(fparticles[i].pos.z - bounds.lower.z) >> 12];
+                ++x_counter[(fparticles[i].pos.x - bounds.lower.x) >> offset];
+                ++y_counter[(fparticles[i].pos.y - bounds.lower.y) >> offset];
+                ++z_counter[(fparticles[i].pos.z - bounds.lower.z) >> offset];
             }
             /*if (x_counter.size() > 1 || y_counter.size() > 1 || z_counter.size() > 1)
                 return false;*/
+            if (x_counter.size() <= 1 && y_counter.size() <= 1 && z_counter.size() <= 1) {
+                device::FPKDLet treelet;
+                treelet.begin = begin;
+                treelet.end = end;
+                treelet.bounds = bounds;
+                /*treelet.bounds.lower -= scaled_radius;
+                treelet.bounds.upper += scaled_radius;*/
 
-            device::FPKDLet treelet;
-            treelet.begin = begin;
-            treelet.end = end;
-            treelet.bounds = bounds;
-            /*treelet.bounds.lower -= scaled_radius;
-            treelet.bounds.upper += scaled_radius;*/
-
-            std::lock_guard<std::mutex> lock(resultMutex);
-            treelets.push_back(treelet);
-            prefix_counter.push_back(glm::uvec3(x_counter.size(), y_counter.size(), z_counter.size()));
+                std::lock_guard<std::mutex> lock(resultMutex);
+                treelets.push_back(treelet);
+                prefix_counter.push_back(glm::uvec3(x_counter.size(), y_counter.size(), z_counter.size()));
+            } else {
+                std::lock_guard<std::mutex> lock(resultMutex);
+                remaining_jobs.push(std::make_tuple(begin, end, offset));
+            }
             return true;
         });
+
+    while (!remaining_jobs.empty()) {
+        auto rj = remaining_jobs.top();
+        remaining_jobs.pop();
+        partitionRecursively<device::FPKDParticle, device::box3u32>(fparticles, std::get<0>(rj), std::get<1>(rj),
+            [&](size_t begin, size_t end, bool force, device::box3u32 const& bounds) {
+                /*bool makeLeaf() :*/
+                const size_t size = end - begin;
+                if (size > maxSize && !force)
+                    return false;
+
+                auto const rj_offset = std::get<2>(rj) + 1;
+                std::unordered_map<uint32_t, uint32_t> x_counter;
+                std::unordered_map<uint32_t, uint32_t> y_counter;
+                std::unordered_map<uint32_t, uint32_t> z_counter;
+                for (size_t i = begin; i < end; ++i) {
+                    ++x_counter[(fparticles[i].pos.x - bounds.lower.x) >> rj_offset];
+                    ++y_counter[(fparticles[i].pos.y - bounds.lower.y) >> rj_offset];
+                    ++z_counter[(fparticles[i].pos.z - bounds.lower.z) >> rj_offset];
+                }
+                /*if (x_counter.size() > 1 || y_counter.size() > 1 || z_counter.size() > 1)
+                    return false;*/
+                if (x_counter.size() <= 1 && y_counter.size() <= 1 && z_counter.size() <= 1) {
+                    device::FPKDLet treelet;
+                    treelet.begin = begin;
+                    treelet.end = end;
+                    treelet.bounds = bounds;
+                    /*treelet.bounds.lower -= scaled_radius;
+                    treelet.bounds.upper += scaled_radius;*/
+
+                    std::lock_guard<std::mutex> lock(resultMutex);
+                    treelets.push_back(treelet);
+                    prefix_counter.push_back(glm::uvec3(x_counter.size(), y_counter.size(), z_counter.size()));
+                } else {
+                    std::lock_guard<std::mutex> lock(resultMutex);
+                    remaining_jobs.push(std::make_tuple(begin, end, rj_offset));
+                }
+                return true;
+            });
+    }
 
     auto const pc = std::count_if(
         prefix_counter.begin(), prefix_counter.end(), [](auto const& el) { return el.x > 1 || el.y > 1 || el.z > 1; });

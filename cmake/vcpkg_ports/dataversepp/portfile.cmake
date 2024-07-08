c15e56b1b892afa1606eb3075610e459fde3f6e6
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2024-07-08 17:07:55 +0200

    dataverse package update

--- cmake/vcpkg_ports/dataversepp/portfile.cmake
+++ cmake/vcpkg_ports/dataversepp/portfile.cmake
@@ -3,19 +3,16 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO UniStuttgart-VISUS/dataversepp
-    REF "master"
-    SHA512 419646e48403e0b8c7bcefd1583bb985203088218c75df756c0ecdf2cdb64f37afc33dcd715b239fb9d73210564433827d2a8abf5310cf6cb66eea5eb5da1927
+    REF 1bf1718f0e681ef01173490d50877641bd410cc2 # master on 2024-06-28
+    SHA512 140c0e0882a45e8d104d426910f5710df27283fbb3e87714b3e31c08b72c4544842d35fba7b6718b9bc38377ff519eab990783dfd35ed2fc4fa8c0bc33ea3c94
     HEAD_REF master
-    PATCHES
-      01_curl.patch
-      02_curl.patch
-      03_curl.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-      -DFETCHCONTENT_FULLY_DISCONNECTED=OFF
+      -DDATAVERSE_BuildCli=OFF
+      -DDATAVERSE_DownloadThirdParty=OFF
       -DDATAVERSE_BuildTests=OFF
       -DDATAVERSE_Unicode=OFF
 )
